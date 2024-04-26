148313da2f82eee87719356151293c48cafcb054
KKN21 <karlkenneth.neuhaus@gmail.com>
2024-04-26 22:58:07 +0300

    Week7

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Enter the operation (+/-)");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter X");
+int firstNum = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Y");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-':
+        Subtraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+
+}
+
+static void Addition(int X, int Y)
+{
+    Console.WriteLine($"{X} + {Y} = {X + Y}");
+}
+
+static void Subtraction(int X, int Y)
+{
+    Console.WriteLine($"{X} - {Y} = {X - Y}");
+}
